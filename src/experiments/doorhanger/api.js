c170351060a534d3715d4ce97eac706dc31c05e9
ahounsel <austin.hounsel@gmail.com>
2019-08-23 15:04:43 -0700

    Closes #18: Add SUMO page link in notification

--- src/experiments/doorhanger/api.js
+++ src/experiments/doorhanger/api.js
@@ -61,7 +61,7 @@ class DoorhangerEventEmitter extends EventEmitter {
       autofocus: true,
       name: "More secure, encrypted DNS lookups",
       popupIconURL: "chrome://browser/skin/connection-secure.svg",
-      learnMoreURL: "https://duckduckgo.com",
+      learnMoreURL: "https://support.mozilla.org/en-US/kb/firefox-dns-over-https"
     };
     recentWindow.PopupNotifications.show(browser, "doh-first-time", "<> Firefox now sends your DNS lookups over an encrypted connection provided by a trusted partner. This helps protect against phishing, malware, and surveillance.", null, primaryAction, secondaryActions, options);
   }
